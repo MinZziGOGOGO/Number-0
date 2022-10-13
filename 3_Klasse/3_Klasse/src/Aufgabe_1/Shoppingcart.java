6c8b2860e8113408c5fcafab7dea3145627220b9
marti <marti@LAPTOP-8QOKL97D>
2022-10-13 10:27:05 +0200

    Finishing

--- 3_Klasse/3_Klasse/src/Aufgabe_1/Shoppingcart.java
+++ 3_Klasse/3_Klasse/src/Aufgabe_1/Shoppingcart.java
@@ -8,8 +8,11 @@ public class Shoppingcart {
 	public static void main(String[] args) {
 		ArrayList<Article> shoppingCart = new ArrayList<>();
 		
+		DVD item1 = new DVD(4523,25.33,30,"Hulk",COUNTRYCODE.at);
+		shoppingCart.add(item1);
+
 		DVD item2 = new DVD(123456,34.99,20,"IronMan",COUNTRYCODE.at);
 		shoppingCart.add(item2);
 		
-System.out.println("Hallo");	}
+	}
 }
