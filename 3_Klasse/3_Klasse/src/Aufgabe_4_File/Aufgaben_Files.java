0c9f3e0b5bb454038e969d3aeeae69918ee37e68
marti <marti@10.10.218.13>
2023-02-02 11:45:23 +0100

    File update

--- 3_Klasse/3_Klasse/src/Aufgabe_4_File/Aufgaben_Files.java
+++ 3_Klasse/3_Klasse/src/Aufgabe_4_File/Aufgaben_Files.java
@@ -5,27 +5,31 @@ import java.sql.Date;
 
 public class Aufgaben_Files {
 
-		public static void main(String[] args) {
-			File file = new File("C:\\Users\\marti\\OneDrive\\Bilder\\Screenshots");
+	public static void main(String[] args) {
+		File file = new File("C:\\Users\\marti\\OneDrive\\Bilder\\Screenshots");
+
+		File[] contents = file.listFiles();
+
+		for (int i = 0; i < contents.length; i++) {
+			double size = contents[i].length() / 1024;
+
+			long lastModified = contents[i].lastModified();
+			Date date = new Date(lastModified);
+
+			String extension = contents[i].toPath().toString().substring(contents[i].toString().indexOf("."));
 			
-			File[] contents = file.listFiles();
-			for (int i = 0; i < contents.length; i++) {
-				double size = contents[i].length() / 1024;
-				
-				long lastModified = contents[i].lastModified();
-			    Date date = new Date(lastModified);
-			    
-			    String ext1 = contents[i].toPath().toString().substring(contents[i].toString().indexOf("."));
-				
-			    System.out.printf("Name: %s%n",contents[i].getName());
-			    System.out.printf("Path: %s%n",contents[i].getAbsolutePath());
-			    System.out.printf("Extension: %s%n",ext1);
-			    System.out.printf("Date: %s%n",date);
-			    System.out.printf("Size: %s kb%n",size);
-			    System.out.println();
-			    System.out.println();
-			}
-			System.out.printf("Amount of Files in the Directory: %d", contents.length);
-			
+			ausgabe(contents, i, size, date, extension);
 		}
+		System.out.printf("Amount of Files in the Directory: %d", contents.length);
 	}
+
+	private static void ausgabe(File[] contents, int i, double size, Date date, String extension) {
+		System.out.printf("Name: %s%n", contents[i].getName());
+		System.out.printf("Path: %s%n", contents[i].getAbsolutePath());
+		System.out.printf("Extension: %s%n", extension);
+		System.out.printf("Date: %s%n", date);
+		System.out.printf("Size: %s kb%n", size);
+		System.out.println();
+		System.out.println();
+	}
+}
